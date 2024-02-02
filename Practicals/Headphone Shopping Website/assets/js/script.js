e6083f17436293f5ce954bd02d621d80adc62b21
(Student  C00282149) Caitlin Brennan <C00282149@setu.ie>
2024-02-02 12:35:29 +0000

    links in shop finished

---
+++ Practicals/Headphone Shopping Website/assets/js/script.js
@@ -0,0 +1,9 @@
+let airpod1 = new Image();
+airpod1 = "assets/imgs/Airpods Gen 1";
+let airpod2 = new Image();
+airpod2 = "assets/imgs/Airpods Gen 2";
+let airpod3 = new Image();
+airpod3 = "assets/imgs/Airpods Gen 3";
+let airpodPro = new Image();
+airpodPro = "assets/img/Airpods Pro";
+var infoText;
