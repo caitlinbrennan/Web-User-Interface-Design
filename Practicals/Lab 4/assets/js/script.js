f8db0247d556cb97f4fa8b1a0a68a78798e13c44
LabUser <c00282149@setu.ie>
2024-02-15 12:35:10 +0000

    responsive menu added

---
+++ Practicals/Lab 4/assets/js/script.js
@@ -0,0 +1,57 @@
+/*let airpod1 = new Image();
+airpod1 = "assets/imgs/Airpods Gen 1";
+let airpod2 = new Image();
+airpod2 = "assets/imgs/Airpods Gen 2";
+let airpod3 = new Image();
+airpod3 = "assets/imgs/Airpods Gen 3";
+let airpodPro = new Image();
+airpodPro = "assets/img/Airpods Pro";
+var infoText;*/
+
+function openAmazonAirpods(){
+    window.open('https://www.amazon.co.uk/Apple-Airpods-Charging-latest-Model/dp/B07PZR3PVB/ref=sr_1_3?adgrpid=1181975898019321&hvadid=73873694020733&hvbmt=be&hvdev=c&hvlocphy=161371&hvnetw=o&hvqmt=e&hvtargid=kwd-73873627211678%3Aloc-92&hydadcr=25256_1958878&keywords=airpods+generation+1&qid=1706875382&sr=8-3');
+}
+function openAppleAirpods2(){
+    window.open('https://www.apple.com/ie/airpods-2nd-generation/');
+}
+function openAppleAirpods3(){
+    window.open('https://www.apple.com/ie/airpods-3rd-generation/');
+}
+function openAppleAirpodsPro(){
+    window.open('https://www.apple.com/ie/airpods-pro/');
+}
+
+function openSamsungFE(){
+    window.open('https://shop.samsung.com/ie/buds-fe-graphite?code=TLGOOGLE&merchant=20000&cid=uk_paid_affiliate_awin-Hatch_36484465067_none_Lead+Generation+(Content)_0_none_none&sv1=affiliate&sv_campaign_id=155280&awc=20000_1706876166_d4e3e964452c573f5290300b2f3e7b44');
+}
+function openAmazonSamsungLive(){
+    window.open('https://www.amazon.co.uk/Samsung-Wireless-Earphones-Manufacturer-Warranty-Mystic-Black/dp/B0BDGK6FTB/ref=sr_1_3?adgrpid=1184174925306539&hvadid=74011132721274&hvbmt=be&hvdev=c&hvlocphy=161371&hvnetw=o&hvqmt=e&hvtargid=kwd-74011076599063%3Aloc-92&hydadcr=3739_1860191&keywords=galaxy%2Bbuds%2Blive&qid=1706876453&sr=8-3&th=1');
+}
+function openAmazonSamsungPro(){
+    window.open('https://www.amazon.co.uk/Samsung-Galaxy-Buds-Pro-Headphones-Black/dp/B08PW3RT3N?ref_=ast_sto_dp');
+}
+function openSamsung2Pro(){
+    window.open('https://shop.samsung.com/ie/galaxy-buds2-pro-bora-purple?code=TLGOOGLE&merchant=20000&cid=uk_paid_affiliate_awin-Hatch_33640932421_none_Lead+Generation+(Content)_0_none_none&sv1=affiliate&sv_campaign_id=155280&awc=20000_1706876414_848cd541fa63ce09afe9b19f21db7fe6');
+}
+
+function openSamsung2(){
+    window.open('https://shop.samsung.com/ie/galaxy-buds2-olive?code=TLGOOGLE&merchant=20000&cid=uk_paid_affiliate_awin-Hatch_30310162013_none_Lead+Generation+(Content)_0_none_none&sv1=affiliate&sv_campaign_id=155280&awc=20000_1706876402_edb50b9d5b8a82b0e540e4d79ef161c1');
+}
+function openCurrysSonyXM3(){
+    window.open('https://www.currys.ie/products/sony-wh1000xm4-wireless-bluetooth-noisecancelling-headphones-blue-10227201.html?sv1=affiliate&sv_campaign_id=155280&awc=5297_1706876978_e24e5b8f93ce7dbc2ba4efaeb3aabad7&utm_source=Hatch+BV&utm_medium=Affiliate&utm_campaign=Affiliate');
+}
+function openAmazonSonyXM4(){
+    window.open('https://www.amazon.co.uk/Sony-WH-1000XM3-Wireless-Cancelling-Headphones-Black/dp/B07GDR2LYK/ref=sr_1_4?adgrpid=1175378830004467&hvadid=73461375312792&hvbmt=be&hvdev=c&hvlocphy=142382&hvnetw=o&hvqmt=e&hvtargid=kwd-73461317450863%3Aloc-92&hydadcr=18274_1846603&keywords=sony+xm3&qid=1706877069&sr=8-4');
+}
+function openCurrysSony720N(){
+    window.open('https://www.currys.ie/products/sony-whch720n-wireless-bluetooth-noisecancelling-headphones-blue-10247673.html?sv1=affiliate&sv_campaign_id=155280&awc=5297_1706877147_a3e21e177a5917a702d15cb88ad4d9ae&utm_source=Hatch+BV&utm_medium=Affiliate&utm_campaign=Affiliate');
+}
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
