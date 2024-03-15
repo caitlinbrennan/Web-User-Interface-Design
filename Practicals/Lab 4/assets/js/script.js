6a19f3d6dd4617e2a80f1883aab3395ef7d27627
Caitlin-Brennan <116352640+Caitlin-Brennan@users.noreply.github.com>
2024-03-15 21:38:38 +0000

    comparison page + reviews finished

--- Practicals/Lab 4/assets/js/script.js
+++ Practicals/Lab 4/assets/js/script.js
@@ -1,4 +1,4 @@
-function openAmazonAirpods(){
+function openAppleAirpods(){
     window.open('https://www.amazon.co.uk/Apple-Airpods-Charging-latest-Model/dp/B07PZR3PVB/ref=sr_1_3?adgrpid=1181975898019321&hvadid=73873694020733&hvbmt=be&hvdev=c&hvlocphy=161371&hvnetw=o&hvqmt=e&hvtargid=kwd-73873627211678%3Aloc-92&hydadcr=25256_1958878&keywords=airpods+generation+1&qid=1706875382&sr=8-3');
 }
 function openAppleAirpods2(){
@@ -14,10 +14,10 @@ function openAppleAirpodsPro(){
 function openSamsungFE(){
     window.open('https://shop.samsung.com/ie/buds-fe-graphite?code=TLGOOGLE&merchant=20000&cid=uk_paid_affiliate_awin-Hatch_36484465067_none_Lead+Generation+(Content)_0_none_none&sv1=affiliate&sv_campaign_id=155280&awc=20000_1706876166_d4e3e964452c573f5290300b2f3e7b44');
 }
-function openAmazonSamsungLive(){
+function openSamsungLive(){
     window.open('https://www.amazon.co.uk/Samsung-Wireless-Earphones-Manufacturer-Warranty-Mystic-Black/dp/B0BDGK6FTB/ref=sr_1_3?adgrpid=1184174925306539&hvadid=74011132721274&hvbmt=be&hvdev=c&hvlocphy=161371&hvnetw=o&hvqmt=e&hvtargid=kwd-74011076599063%3Aloc-92&hydadcr=3739_1860191&keywords=galaxy%2Bbuds%2Blive&qid=1706876453&sr=8-3&th=1');
 }
-function openAmazonSamsungPro(){
+function openSamsungPro(){
     window.open('https://www.amazon.co.uk/Samsung-Galaxy-Buds-Pro-Headphones-Black/dp/B08PW3RT3N?ref_=ast_sto_dp');
 }
 function openSamsung2Pro(){
@@ -28,38 +28,89 @@ function openSamsung2(){
     window.open('https://shop.samsung.com/ie/galaxy-buds2-olive?code=TLGOOGLE&merchant=20000&cid=uk_paid_affiliate_awin-Hatch_30310162013_none_Lead+Generation+(Content)_0_none_none&sv1=affiliate&sv_campaign_id=155280&awc=20000_1706876402_edb50b9d5b8a82b0e540e4d79ef161c1');
 }
 function openCurrysSonyXM3(){
-    window.open('https://www.currys.ie/products/sony-wh1000xm4-wireless-bluetooth-noisecancelling-headphones-blue-10227201.html?sv1=affiliate&sv_campaign_id=155280&awc=5297_1706876978_e24e5b8f93ce7dbc2ba4efaeb3aabad7&utm_source=Hatch+BV&utm_medium=Affiliate&utm_campaign=Affiliate');
+    window.open('https://www.amazon.co.uk/Sony-WH-1000XM3-Wireless-Cancelling-Headphones-Black/dp/B07GDR2LYK/ref=sr_1_4?adgrpid=1175378830004467&dib=eyJ2IjoiMSJ9.2IakricV6OD3nxBt5jXgf5w4yqZ7UOsSCR9lALZjDDidxfv_vCMmdrrAikmc8BYI-gaTda56tlJIVwsC6PfZ5lxSdAdwqAWP4zLOVIb8HGJ3fqlKX_g684j7qUkuR3hENZJkwC-BzFV44_-C9mQVMOayIGpmATNmbJwdXcjSbZwwOTqxK0RYATMQ1bs_XO3ixKXxwlUhzGDyk_Ozuj914H9NcSYqZLPST-cEZrp0zrA.9AGX-R8thVelat-uv1f8lZzdna41S04ID0qf-o0bKIE&dib_tag=se&hvadid=73461375312793&hvbmt=be&hvdev=c&hvlocphy=142380&hvnetw=o&hvqmt=e&hvtargid=kwd-73461317450863%3Aloc-92&hydadcr=18274_1846602&keywords=sony+xm3&qid=1710538273&sr=8-4');
 }
 function openAmazonSonyXM4(){
     window.open('https://www.amazon.co.uk/Sony-WH-1000XM3-Wireless-Cancelling-Headphones-Black/dp/B07GDR2LYK/ref=sr_1_4?adgrpid=1175378830004467&hvadid=73461375312792&hvbmt=be&hvdev=c&hvlocphy=142382&hvnetw=o&hvqmt=e&hvtargid=kwd-73461317450863%3Aloc-92&hydadcr=18274_1846603&keywords=sony+xm3&qid=1706877069&sr=8-4');
 }
 function openCurrysSony720N(){
-    window.open('https://www.currys.ie/products/sony-whch720n-wireless-bluetooth-noisecancelling-headphones-blue-10247673.html?sv1=affiliate&sv_campaign_id=155280&awc=5297_1706877147_a3e21e177a5917a702d15cb88ad4d9ae&utm_source=Hatch+BV&utm_medium=Affiliate&utm_campaign=Affiliate');
+    window.open('https://www.amazon.co.uk/Sony-WH-CH720N-Cancelling-Bluetooth-Headphones-Black/dp/B0BTDX26B2/ref=sr_1_3?adgrpid=1181976044647865&dib=eyJ2IjoiMSJ9.EwcQFDXnxKWMak1D32FS0EOLPEyY1eP0gqcJgWpqFQYwNqADkwWMWNv_7a5DluVJf4YAoRo98ZfhOpuQyUlfkRCnJQ8toWo6njtfheK3mBxnF9pVIaHeEagtVhi-_EobU4-WmHfvhQv8vRi_L9qZWSsw8f3OefexelNMgTqhauEY7cAhKVMKkeRFrn-YonG6JB9aqJg-9YG-GAZ2EoMzP1kn31EBieEwbcCHcMF3UjE.tODfWgWsHyEOTGOt8XkECZeVDMqeAV6J4tIMVW7o4uk&dib_tag=se&hvadid=73873731013778&hvbmt=be&hvdev=c&hvlocphy=142380&hvnetw=o&hvqmt=e&hvtargid=kwd-73873733795461%3Aloc-92&hydadcr=27734_2227843&keywords=sony+720n&qid=1710538449&sr=8-3');
 }
+
 
 function showInfo(selectId) {
     var selectElement = document.getElementById(selectId);
     var selectedItemValue = selectElement.value;
     var infoElementId = "info" + selectId.slice(-1); // Extracting the number from selectId to form info element ID
     var infoElement = document.getElementById(infoElementId);
-    var itemInfoElement = document.getElementById("itemInfo" + selectId.slice(-1));
+    var itemNameElement = document.getElementById("itemInfo" + selectId.slice(-1));
+    var itemPriceElement = document.getElementById("itemPrice" + selectId.slice(-1));
+    var itemProsElement = document.getElementById("itemPros" + selectId.slice(-1));
+    var itemConsElement = document.getElementById("itemCons" + selectId.slice(-1));
+    var itemLinkElement = document.getElementById("itemLink" + selectId.slice(-1));
 
-    var itemInfo = {
-        "1": "Information about Apple Airpods.",
-        "2": "Information about Apple Airpods Gen 2.",
-        "3": "Information about Apple Airpods Gen 3.",
-        "4": "Information about Apple Airpods Pro.",
-        "5": "Information about Samsung Galaxy Buds FE.",
-        "6": "Information about Samsung Galaxy Buds Live.",
-        "7": "Information about Samsung Galaxy Buds Pro.",
-        "8": "Information about Samsung Galaxy Buds2 Pro.",
-        "9": "Information about Samsung Galaxy Buds2.",
-        "10": "Information about Sony WH-1000XM3.",
-        "11": "Information about Sony WH-1000XM4."
+    var itemName = {
+        "1": "Apple Airpods Generation 1",
+        "2": "Apple Airpods Generation 2",
+        "3": "Apple Airpods Gen 3",
+        "4": "Apple Airpods Pro",
+        "5": "Samsung Galaxy Buds FE",
+        "6": "Samsung Galaxy Buds Live",
+        "7": "Samsung Galaxy Buds Pro",
+        "8": "Samsung Galaxy Buds2 Pro",
+        "9": "Samsung Galaxy Buds2",
+        "10": "Sony WH-1000XM3",
+        "11": "Sony WH-1000XM4",
+        "12": "Sony WH-CH720N"
     };
+    var itemPrice = {
+        "1": "Amazon - €118.92",
+        "2": "Apple - €149.00",
+        "3": "Apple - €209.00",
+        "4": "Apple - €279.00",
+        "5": "Samsung - €89.00",
+        "6": "Amazon - €69.82",
+        "7": "Amazon - €125.28",
+        "8": "Samsung - €239.00",
+        "9": "Samsung - €109.00",
+        "10": "Currys - €299.00",
+        "11": "Amazon - €245.16",
+        "12": "Currys - €119.99"
+    }
+    var itemPros = {
+        "1": "- Comfortable",
+        "2": "- Lightweight",
+        "3": "- Water-resistant",
+        "4": "- Very Comfortable",
+        "5": "- Great for Price",
+        "6": "- Good battery life",
+        "7": "- Good noise cancellation",
+        "8": "- Long battery life",
+        "9": "- Good functionality",
+        "10": "- 30 Hour battery life",
+        "11": "- Multi-device pairing",
+        "12": "- Good sound balance"    
+    }
+    var itemCons = {
+        "1": "- Easy to Lose",
+        "2": "- Not waterproof",
+        "3": "- Not secure fit",
+        "4": "- Not best sound quality",
+        "5": "- Awkward fit",
+        "6": "- Does not fit every ear",
+        "7": "- Uncomfortable fit",
+        "8": "- Poor quality",
+        "9": "- Poor quality",
+        "10": "- Mid call quality",
+        "11": "- Not much changed from XM3",
+        "12": "- Sound can leak"    
+    }
 
-    if (selectedItemValue && itemInfo[selectedItemValue]) {
-        itemInfoElement.textContent = itemInfo[selectedItemValue];
+    if (selectedItemValue && itemName[selectedItemValue]) {
+        itemNameElement.textContent = itemName[selectedItemValue];
+        itemPriceElement.textContent = itemPrice[selectedItemValue];
+        itemProsElement.textContent = itemPros[selectedItemValue];
+        itemConsElement.textContent = itemCons[selectedItemValue];
         infoElement.style.display = "block";
     } else {
         infoElement.style.display = "none";
