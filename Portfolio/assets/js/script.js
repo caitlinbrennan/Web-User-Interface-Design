85b412b7270ea15a122aa7f6b12fda79d0d34711
(Student  C00282149) Caitlin Brennan <C00282149@setu.ie>
2024-04-18 10:41:04 +0100

    bigger monitor responsive

--- Portfolio/assets/js/script.js
+++ Portfolio/assets/js/script.js
@@ -5,6 +5,12 @@ function scrollDown(){
 function openGithub(){
     window.open("https://github.com/caitlinbrennan");
 }
+function openDiscord(){
+    window.open("https://www.discordapp.com/users/811303542475456542");
+}
+function openLinkedIn(){
+    window.open("https://linkedin.com/in/caitlin-brennan-1bb736304/");
+}
 
 function openSite(){
     window.open("https://c00282149.candept.com/Lab%205/index.html");
